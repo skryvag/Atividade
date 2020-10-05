6559a6af30d376b046a8f952d1bc814232d9b1a7
Artyom <61833755+skryvag@users.noreply.github.com>
2020-10-05 11:16:56 -0300

    Create Questão 1.py

---
+++ Questão 1.py
@@ -0,0 +1,24 @@
+def menu_inicial():
+    print('Programa para Conversão de Temperaturas')
+    print('1. Converter de Celsius para Fahrenheit')
+    print('2. Converter de Fahrenheit para Celsius')
+
+def cel_fah():
+    C = float(input('Entre com a temperatura em graus Celsius: '))
+    F = C * (9 / 5) + 32
+    print('Valor em Fahrenheit: {0}°F'.format(F))
+
+def fah_cel():
+    F = float(input('Entre com a temperatura em graus Fahrenheit: '))
+    C = (F - 32) * (5 / 9)
+    print('Valor em Celsius: {0}°C'.format(C))
+
+if __name__=='__main__':
+    menu_inicial()
+    escolha = input('Escolha o tipo de conversão que deseja realizar: ')
+
+    if escolha == '1':
+        cel_fah()
+
+    if escolha == '2':
+        fah_cel()
